a74fec04a70f9e38fd128757246037ec8b7154c5
Nikita <dolganov.alfa@gmail.com>
2024-03-23 17:42:10 +0300

    1

---
+++ data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name = input('Введите ваше имя ')
+    return name
+
+def surname_data():
+    surname = input('Введите фамилию ')
+    return surname
+
+def phone_data():
+    phone = input('Введите номер телефона ')
+    return phone
+
+def address_data():
+    address = input('Введите ваш адрес ')
+    return address
+
+def delete_data():
+    delete = input('Введите что удалить ')
+    return delete
